9e3e379f55ee2a9f31a071617b3d1b140c77ccb0
Lauriane Anthony <lauriane.anthony@gmail.com>
2018-12-18 16:28:35 +0100

    format session to add start time and end time - display sessions group by date

--- src/component/session/SessionList.js
+++ src/component/session/SessionList.js
@@ -1,71 +1,53 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getFilteredSessions, sessionActions } from "./core"
-import { createSelector } from "reselect"
-import SessionItem from "./SessionItem"
-import Grid from "@material-ui/core/Grid"
-import { getFilteredSchedules, scheduleActions } from "../schedule/core"
-import { withStyles } from "@material-ui/core"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSessionsGroupByDate, sessionActions } from "./core";
+import SessionItem from "./SessionItem";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core";
 
 const styles = theme => ({
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
-            width: 900,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  }
 });
 
-
 class SessionList extends Component {
+  componentDidMount() {
+    this.props.getSessions();
+  }
 
-    componentDidMount() {
-        this.props.getSessions()
-        this.props.getSchedules()
-    }
+  render() {
+    const { sessionsByDate, classes } = this.props;
+    if (!sessionsByDate) return "Data loading";
 
-    render() {
-        const {sessions, schedule, classes} = this.props;
+    return sessionsByDate.map((current, key) => (
+      <Grid container spacing={24} className={classes.layout} key={key}>
+        <Grid item xs>
+          {current.date}
+        </Grid>
 
-        if(!sessions || !schedule) return "Data loading"
-
-        console.log(schedule)
-
-        return schedule.map(scheduleItem =>
-            <Grid container spacing={24} className={classes.layout} key={scheduleItem.date}>
-                <Grid item xs >
-                    { scheduleItem.date }
-                </Grid>
-
-                {
-                    scheduleItem.timeslots.map(timeslot =>
-                        timeslot.sessions.map(timeslotSessions =>
-                            timeslotSessions.items.map(sessionId =>
-                                <SessionItem
-                                    key={sessionId}
-                                    session={sessions[sessionId]}/>
-                            )
-                        )
-                    )
-                }
-
-        </Grid>)
-    }
+        {current.sessions.map((session, key) => (
+          <SessionItem key={key} session={session} />
+        ))}
+      </Grid>
+    ));
+  }
 }
 
-const mapStateToProps = createSelector(
-    getFilteredSessions,
-    getFilteredSchedules,
-    (sessions, schedule) => ({
-        sessions,
-        schedule
-    })
-)
+const mapStateToProps = state => ({
+  sessionsByDate: getSessionsGroupByDate(state)
+});
 
-const mapDispatchToProps = Object.assign({}, sessionActions, scheduleActions)
+const mapDispatchToProps = Object.assign({}, sessionActions);
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SessionList))
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(SessionList));
