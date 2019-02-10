5b1ce26c06ce088af20639505aa468af270e4447
Lauriane Anthony <lauriane.anthony@gmail.com>
2019-02-10 12:55:39 +0100

    split sessions and session

--- src/component/session/core/sessionReducer.js
+++ src/component/session/core/sessionReducer.js
@@ -1,66 +1,24 @@
-import {
-    GET_SESSION_ERROR,
-    GET_SESSION_SUCCESS,
-    GET_SESSIONS_ERROR,
-    GET_SESSIONS_SUCCESS,
-    SET_SELECTED_SESSION,
-    SET_SESSION_FILTER,
-    GET_SESSIONS_LOADING
-} from './sessionActionTypes'
+import { GET_SESSION_ERROR, SET_SELECTED_SESSION } from './sessionActionTypes'
 
 const initState = {
-    list: {},
-    filter: null,
     selected: null,
-    errorSessionLoad: null,
-    errorSessionsLoad: null,
-    loading: false
+    errorSessionLoad: null
 }
 
 const sessionReducer = (state = initState, { payload, type }) => {
     switch (type) {
-        case GET_SESSION_SUCCESS:
-            return {
-                ...state,
-                list: {
-                    ...state.list,
-                    ...payload
-                }
-            }
-        case GET_SESSIONS_LOADING:
-            return {
-                ...state,
-                loading: true
-            }
-        case GET_SESSIONS_SUCCESS:
-            return {
-                ...state,
-                list: payload,
-                loading: false
-            }
-        case SET_SESSION_FILTER:
-            return {
-                ...state,
-                filter: payload
-            }
         case SET_SELECTED_SESSION:
             return {
                 ...state,
                 selected: payload
             }
         case GET_SESSION_ERROR:
-            console.error(payload)
             return {
                 ...state,
                 errorSessionLoad: payload,
                 loading: false
             }
-        case GET_SESSIONS_ERROR:
-            console.error(payload)
-            return {
-                ...state,
-                errorSessionsLoad: payload
-            }
+
         default:
             return state
     }
