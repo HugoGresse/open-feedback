e42f42e72359d53550921961f2d7eb74a7d25109
Lauriane Anthony <lauriane.anthony@gmail.com>
2019-02-10 17:51:08 +0100

    home

---
+++ src/component/home/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { COLORS } from '../../constants/colors'
+import logoColorWhite from '../../assets/logo-openfeedback-color&white.png'
+
+const Wrapper = styled.div`
+    background-color: ${COLORS.DARK_GRAY};
+    color: ${COLORS.WHITE};
+    text-align: center;
+    padding: 50px 0 30px 0;
+`
+
+class Footer extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <img height="30" src={logoColorWhite} alt="open feedback" />
+                <p>Copyright 2019</p>
+            </Wrapper>
+        )
+    }
+}
+
+export default Footer
