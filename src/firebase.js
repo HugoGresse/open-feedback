ca8d165a7d5460a3621e00171d01a0331bea2562
Hugo Gresse <hugo.gresse@teads.tv>
2019-02-16 15:49:45 +0100

    Clean warning & error

--- src/firebase.js
+++ src/firebase.js
@@ -5,8 +5,6 @@ import config from './config'
 
 const firebaseMain = firebase.initializeApp(config.firebaseMain)
 
-firebaseMain.firestore().settings({ timestampsInSnapshots: true })
-
 export const authProvider = firebaseMain.auth()
 export const fireStoreMainInstance = firebaseMain.firestore()
 export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp
@@ -18,8 +16,5 @@ export const initFireStoreSchedule = config => {
     }
 
     const firebaseSchedule = firebase.initializeApp(config, 'schedule')
-
-    firebaseSchedule.firestore().settings({ timestampsInSnapshots: true })
-
     fireStoreScheduleInstance = firebaseSchedule.firestore()
 }
