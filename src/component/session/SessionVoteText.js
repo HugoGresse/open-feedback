6f7f90545a8ffb2268d971aa7f3dd082fa6ad896
Lauriane Anthony <lauriane.anthony@gmail.com>
2019-02-28 21:12:13 +0100

    fix horizontal scroll session textarea

--- src/component/session/SessionVoteText.js
+++ src/component/session/SessionVoteText.js
@@ -13,6 +13,7 @@ const styles = theme => ({
         margin: -1
     },
     item: {
+        overflow: 'hidden',
         padding: theme.spacing.unit * 2,
         textAlign: 'center',
         fontSize: '17px',
