21f4e2a90348a83c5d840ae87ce4f0acd9e5381b
Hugo Gresse <hugo.gresse@teads.tv>
2019-06-19 14:08:00 +0200

    Improve comment area

--- src/feedback/session/SessionVoteTextResult.js
+++ src/feedback/session/SessionVoteTextResult.js
@@ -3,20 +3,34 @@ import { withStyles } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 
-const styles = theme => ({})
+const styles = theme => ({
+    date: {
+        color: '#888',
+        fontSize: 14,
+        marginBottom: 0
+    },
+    comment: {
+        borderBottom: '1px solid #e8e8e8',
+        paddingBottom: 15,
+        marginTop: 10,
+        wordBreak: 'break-all',
+        whiteSpace: 'pre-wrap'
+    }
+})
 
 class SessionVoteTextResult extends Component {
     render() {
-        const { result } = this.props
+        const { result, classes } = this.props
 
         return (
             <div>
                 {result.map((item, key) => (
-                    <p key={key}>
-                        <span>{moment(item.updatedAt).fromNow()} - </span>
-
-                        {item.text}
-                    </p>
+                    <div key={key}>
+                        <p className={classes.date}>
+                            {moment(item.updatedAt).fromNow()}
+                        </p>
+                        <p className={classes.comment}>{item.text}</p>
+                    </div>
                 ))}
             </div>
         )
