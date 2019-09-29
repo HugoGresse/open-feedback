3bf3a991b28093b954020d4ea2e8796d275edaf1
Hugo Gresse <hugo.gresse@gmail.com>
2019-09-29 14:18:47 +0200

    Fix Google Analytics + small refacto for main App.js (#136)
    * Refactor main App.js with Feedback App plus remove GA Middleware
    
    * Add GA

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import { Provider } from 'react-redux'
-
 import App from './App'
-import { initGA } from './reactGAMiddlewares'
 import configureStore from './configureStore'
 
-initGA()
 
 const store = configureStore({})
 
