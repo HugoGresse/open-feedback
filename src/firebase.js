bb331827a945d06c2d352944c27721bcb808ae0c
Hugo Gresse <hugo.gresse@gmail.com>
2019-09-29 16:38:24 +0200

    Add appId

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,8 @@ const config = {
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    appId: process.env.REACT_APP_APPID
 }
 
 const firebaseMain = firebase.initializeApp(config)
