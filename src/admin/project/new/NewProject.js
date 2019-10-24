3f5e449281b1027f7f1e4e6a1899b758bf70e672
Hugo Gresse <hugo.gresse@gmail.com>
2019-10-24 15:47:23 +0200

    Plug everything together

--- src/admin/project/new/NewProject.js
+++ src/admin/project/new/NewProject.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Grid from '@material-ui/core/Grid'
 import makeStyles from '@material-ui/core/styles/makeStyles'
-import {COLORS} from '../../../constants/colors'
+import { COLORS } from '../../../constants/colors'
 import Hidden from '@material-ui/core/Hidden'
 import Step1 from './Step1'
 import Step2 from './Step2'
 import Step3 from './Step3'
+import { useDispatch } from 'react-redux'
+import { newProject } from '../core/projectActions'
+import { history } from '../../../App'
 
 const useStyles = makeStyles({
     container: {
@@ -18,45 +21,60 @@ const useStyles = makeStyles({
         background: COLORS.RED_ORANGE
     }
 })
-// TODO : store PROJECTYPE const and replace occurence
-const NewProject = ({onCancel}) => {
+const NewProject = ({ onCancel }) => {
     const classes = useStyles()
+    const dispatch = useDispatch()
 
-    const [currentStep, setCurrentStep] = useState(3)
-
+    const [currentStep, setCurrentStep] = useState(1)
     const [projectName, setProjectName] = useState('')
     const [projectType, setProjectType] = useState('')
-    const [projectConfig, setProjectConfig] = useState({})
 
-    console.log("Peroject: ", projectName, projectType, projectConfig)
+    return (
+        <Grid container className={classes.container}>
+            <Grid item xs={12} sm={9} className={classes.leftContainer}>
+                {currentStep === 1 && (
+                    <Step1
+                        onCancel={onCancel}
+                        onSubmit={projectName => {
+                            setCurrentStep(2)
+                            setProjectName(projectName)
+                        }}
+                    />
+                )}
+                {currentStep === 2 && (
+                    <Step2
+                        onCancel={onCancel}
+                        onSubmit={projectType => {
+                            setCurrentStep(3)
+                            setProjectType(projectType)
+                        }}
+                    />
+                )}
 
-    return <Grid container className={classes.container}>
-        <Grid item xs={12} sm={9} className={classes.leftContainer}>
-            {currentStep === 1 && <Step1 onCancel={onCancel}
-                                         onSubmit={(projectName) => {
-                                             setCurrentStep(2)
-                                             setProjectName(projectName)
-                                         }}/>}
-            {currentStep === 2 && <Step2 onCancel={onCancel}
-                                         onSubmit={(projectType) => {
-                                             setCurrentStep(3)
-                                             setProjectType( projectType)
-                                         }}/>}
-
-            {currentStep === 3 && <Step3 onCancel={onCancel}
-                                         projectType={projectType}
-                                         onSubmit={(data) => {
-                                             setProjectConfig(data)
-                                         }}/>}
-        </Grid>
-
-        <Hidden xsDown>
-            <Grid item sm={3} className={classes.rightContainer}>
-
+                {currentStep === 3 && (
+                    <Step3
+                        onCancel={onCancel}
+                        projectType={projectType}
+                        onSubmit={data => {
+                            dispatch(
+                                newProject({
+                                    name: projectName,
+                                    setupType: projectType,
+                                    config: data
+                                })
+                            ).then(projectId => {
+                                history.push(`/admin/${projectId}`)
+                            })
+                        }}
+                    />
+                )}
             </Grid>
-        </Hidden>
-    </Grid>
 
+            <Hidden xsDown>
+                <Grid item sm={3} className={classes.rightContainer}></Grid>
+            </Hidden>
+        </Grid>
+    )
 }
 
 export default NewProject
