09eed55db1db6d299acbb425d0554f36b9c55b73
Hugo Gresse <hugo.gresse@gmail.com>
2019-11-06 08:05:26 +0100

    fix import in functions

--- functions/src/firebaseInit.ts
+++ functions/src/firebaseInit.ts
@@ -1,11 +1,9 @@
-import * as admin from "firebase-admin"
-import serviceAccount from "../serviceAccountKey.json";
-import {ServiceAccount} from "firebase-admin";
+import * as admin from 'firebase-admin'
+import serviceAccount from '../serviceAccountKey.json'
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 })
-
 
 export const firestore = admin.firestore()
