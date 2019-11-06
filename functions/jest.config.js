a055eb4ea5a523e8c8bd0ad35f4261fea00db08d
Hugo Gresse <hugo.gresse@gmail.com>
2019-11-06 07:56:29 +0100

    Migrate functions to typescript (#188)
    * migrate functions to typescript
    * Add test on functions
    * Run functions test on github ci
    * bump lodash
    * add fake key to service account

---
+++ functions/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    roots: ['<rootDir>/test'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.test.json'
+        }
+    }
+}
