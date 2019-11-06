be9cd967048da0940794aacf96c9530e32a691bf
Hugo Gresse <hugo.gresse@gmail.com>
2019-11-06 08:46:53 +0100

    fix null session error on functions

--- functions/src/aggregateVotes.ts
+++ functions/src/aggregateVotes.ts
@@ -1,29 +1,47 @@
-import * as  functions from 'firebase-functions'
-import * as firebase from "firebase"
+import * as functions from 'firebase-functions'
+import * as firebase from 'firebase'
 import DocumentData = firebase.firestore.DocumentData
-import {firestore} from "./firebaseInit"
+import { firestore } from './firebaseInit'
 
 export const aggregateVotesCreate = functions.firestore
     .document('/projects/{projectId}/userVotes/{voteId}')
-    .onCreate((snapshot) => {
-        return incrementVoteAggregate(firestore, snapshot.id, snapshot.data(), +1)
+    .onCreate(snapshot => {
+        return incrementVoteAggregate(
+            firestore,
+            snapshot.id,
+            snapshot.data(),
+            +1
+        )
     })
 
 export const aggregateVotesDelete = functions.firestore
     .document('/projects/{projectId}/userVotes/{voteId}')
-    .onDelete((snapshot) => {
-        return incrementVoteAggregate(firestore, snapshot.id, snapshot.data(), -1)
+    .onDelete(snapshot => {
+        return incrementVoteAggregate(
+            firestore,
+            snapshot.id,
+            snapshot.data(),
+            -1
+        )
     })
 
 export const aggregateVotesUpdate = functions.firestore
     .document('/projects/{projectId}/userVotes/{voteId}')
-    .onUpdate((change) => {
-        return incrementVoteAggregate(firestore, change.after.id, change.after.data(), 1)
+    .onUpdate(change => {
+        return incrementVoteAggregate(
+            firestore,
+            change.after.id,
+            change.after.data(),
+            1
+        )
     })
 
-
-
-export const incrementVoteAggregate = (firestoreDb: FirebaseFirestore.Firestore, newVoteId: string, newVote: DocumentData | undefined, increment: number) => {
+export const incrementVoteAggregate = (
+    firestoreDb: FirebaseFirestore.Firestore,
+    newVoteId: string,
+    newVote: DocumentData | undefined,
+    increment: number
+) => {
     if (
         !newVote ||
         !isIdValid(newVote.projectId) ||
@@ -46,11 +64,7 @@ export const incrementVoteAggregate = (firestoreDb: FirebaseFirestore.Firestore,
         .get()
         .then(snapshot => {
             let aggregatedValue
-                const session = snapshot.data()
-
-            if(!session){
-                return Promise.reject('undefined session')
-            }
+            const session = snapshot.data()
 
             if (newVote.text) {
                 const voteText = {
@@ -60,7 +74,11 @@ export const incrementVoteAggregate = (firestoreDb: FirebaseFirestore.Firestore,
                     userId: newVote.userId
                 }
                 if (increment > 0) {
-                    if (!snapshot.exists || !session[newVote.voteItemId]) {
+                    if (
+                        !snapshot.exists ||
+                        !session ||
+                        !session[newVote.voteItemId]
+                    ) {
                         aggregatedValue = { [newVoteId]: voteText }
                     } else {
                         aggregatedValue = {
@@ -69,7 +87,11 @@ export const incrementVoteAggregate = (firestoreDb: FirebaseFirestore.Firestore,
                         }
                     }
                 } else {
-                    if (!snapshot.exists || !session[newVote.voteItemId]) {
+                    if (
+                        !snapshot.exists ||
+                        !session ||
+                        !session[newVote.voteItemId]
+                    ) {
                         aggregatedValue = {}
                     } else {
                         aggregatedValue = session[newVote.voteItemId]
@@ -77,7 +99,11 @@ export const incrementVoteAggregate = (firestoreDb: FirebaseFirestore.Firestore,
                     }
                 }
             } else {
-                if (!snapshot.exists || !session[newVote.voteItemId]) {
+                if (
+                    !snapshot.exists ||
+                    !session ||
+                    !session[newVote.voteItemId]
+                ) {
                     aggregatedValue = increment
                 } else {
                     aggregatedValue = session[newVote.voteItemId] + increment
@@ -95,7 +121,6 @@ export const incrementVoteAggregate = (firestoreDb: FirebaseFirestore.Firestore,
             console.log('Error getting documents session', err)
             return err
         })
-
 }
 const isIdValid = (id: any): boolean => {
     return id && id.length > 0
