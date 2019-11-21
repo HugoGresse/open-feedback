fb48e35296a52b585931fe5964ae808a74027818
Hugo Gresse <hugo.gresse@gmail.com>
2019-11-21 10:18:40 +0100

    Change invite link

--- functions/src/triggers/userInvite.ts
+++ functions/src/triggers/userInvite.ts
@@ -1,9 +1,9 @@
 import * as functions from 'firebase-functions'
 import admin from 'firebase-admin'
-import send from "../email/send"
+import send from '../email/send'
 import userInvited from '../email/templates/userInvited'
-import {isEmpty} from 'lodash'
-import {Response} from "node-fetch"
+import { isEmpty } from 'lodash'
+import { Response } from 'node-fetch'
 
 // userInviteCreated is called when an user as been invited to a project (to have edit rights).
 // An email is sent to him with the invitation link.
@@ -13,8 +13,8 @@ import {Response} from "node-fetch"
 // > userInviteCreated({projectId: "", projectName: "", originUserName: "", destinationUserInfo: ""})
 export const userInviteCreated = functions.firestore
     .document('/projects-invites/{inviteId}')
-    .onCreate(async (snapshot) => {
-        const {app, mailgun} = functions.config()
+    .onCreate(async snapshot => {
+        const { app, mailgun } = functions.config()
         const inviteId = snapshot.id
         const data = snapshot.data()
 
@@ -23,7 +23,9 @@ export const userInviteCreated = functions.firestore
         }
 
         if (isEmpty(app) || isEmpty(mailgun)) {
-            return Promise.reject(new Error('No config set on "app" or "mailgun"'))
+            return Promise.reject(
+                new Error('No config set on "app" or "mailgun"')
+            )
         }
 
         const sendResult = await send(mailgun, {
@@ -32,16 +34,21 @@ export const userInviteCreated = functions.firestore
             html: userInvited(
                 `${data.originUserName} invited you to become a member of the event "${data.projectName}"`,
                 `Hey ${data.destinationUserInfo},<br/><br/> I invited you to become a member of the OpenFeedback event <b>${data.projectName}</b>. Click on the button below to accept my invitation.`,
-                "Accept the invitation",
-                `${app.url}/admin/invite?id=${inviteId}`)
+                'Accept the invitation',
+                `${app.url}/admin/?inviteId=${inviteId}`
+            )
         })
         if (sendResult instanceof Response && sendResult.ok) {
-            return admin.firestore()
+            return admin
+                .firestore()
                 .collection('projects-invites')
                 .doc(inviteId)
-                .set({
-                    status: 'emailSent'
-                }, {merge: true})
+                .set(
+                    {
+                        status: 'emailSent'
+                    },
+                    { merge: true }
+                )
         }
         return Promise.reject('Function failed due to ??')
     })
