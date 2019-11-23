6ef24471e2caa09b03b8b620f981561f4f849528
Hugo Gresse <hugo.gresse@gmail.com>
2019-11-23 10:45:11 +0100

    Admin:  user invite list (#242)
    * Remove unused dependency in functions/ package.json
    * Display pending invites + remove button + firestore rules updates
    * Redirect user if invite is not destined to him
    * Add user to pending list directly when creating an invite & prevent creating it if already in pending list
    * Update prettier rules

--- src/admin/project/settings/users/usersActions.js
+++ src/admin/project/settings/users/usersActions.js
@@ -1,9 +1,14 @@
-import {fireStoreMainInstance, serverTimestamp} from '../../../../firebase'
+import {fireStoreMainInstance, nowTimestamp, serverTimestamp} from '../../../../firebase'
 import {ADD_NOTIFICATION} from '../../../notification/notificationActionTypes'
-import {GET_USER_DETAILS_SUCCESS, USER_INVITE_GET_SUCCESS, USERS_SET_FILTER} from './usersActionTypes'
-import {getUsersSelector} from './usersSelectors'
+import {
+    GET_USER_DETAILS_SUCCESS, USER_INVITE_ADD,
+    USER_INVITE_GET_SUCCESS, USER_INVITE_REMOVE_SUCCESS,
+    USER_INVITES_GET_SUCCESS,
+    USERS_SET_FILTER
+} from './usersActionTypes'
+import {getPendingInvitesSelector, getUsersSelector} from './usersSelectors'
 import {editProject} from '../../core/projectActions'
-import {getMemberIds, getSelectedProjectSelector} from '../../core/projectSelectors'
+import {getMemberIds, getSelectedProjectIdSelector, getSelectedProjectSelector} from '../../core/projectSelectors'
 import {getUserSelector} from '../../../auth/authSelectors'
 import {getDataFromProviderDataOrUser} from '../../../auth/authActions'
 import {history} from '../../../../App'
@@ -58,23 +63,36 @@ export const removeUserFromProject = (userId) => (dispatch, getState) => {
 }
 
 export const inviteUser = userEmail => (dispatch, getState) => {
+    const pendingInvites = getPendingInvitesSelector(getState())
+    if(pendingInvites.filter(invite => invite.destinationUserInfo === userEmail).length > 0) {
+        dispatch({
+            type: ADD_NOTIFICATION,
+            payload: {
+                type: 'error',
+                message: 'The user is already invited to the event.'
+            }
+        })
+        return Promise.resolve(false)
+    }
+
     const currentUser = getUserSelector(getState())
     const project = getSelectedProjectSelector(getState())
+    const invite = {
+        createdAt: serverTimestamp(),
+        updatedAt: serverTimestamp(),
+        destinationUserInfo: userEmail,
+        originUserId: currentUser.uid,
+        originUserName: currentUser.displayName,
+        originUserPhotoURL: getDataFromProviderDataOrUser(currentUser, 'photoURL'),
+        projectId: project.id,
+        projectName: project.name,
+        status: 'new'
+    }
 
     return fireStoreMainInstance
         .collection('projects-invites')
-        .add({
-            createdAt: serverTimestamp(),
-            updatedAt: serverTimestamp(),
-            destinationUserInfo: userEmail,
-            originUserId: currentUser.uid,
-            originUserName: currentUser.displayName,
-            originUserPhotoURL: getDataFromProviderDataOrUser(currentUser, 'photoURL'),
-            projectId: project.id,
-            projectName: project.name,
-            status: 'new'
-        })
-        .then(() => {
+        .add(invite)
+        .then(docRef => {
             dispatch({
                 type: ADD_NOTIFICATION,
                 payload: {
@@ -82,7 +100,16 @@ export const inviteUser = userEmail => (dispatch, getState) => {
                     message: `The user has been invited to the project and should receive an email very soon.`
                 }
             })
-            return true
+
+            return dispatch({
+                type: USER_INVITE_ADD,
+                payload: {
+                    ...invite,
+                    id: docRef.id,
+                    createdAt: nowTimestamp(),
+                    updatedAt: nowTimestamp()
+                }
+            })
         })
         .catch(error => {
             // eslint-disable-next-line no-console
@@ -109,13 +136,13 @@ export const listenForInvite = inviteId => dispatch => {
         .collection('projects-invites')
         .doc(inviteId)
         .onSnapshot(snapshot => {
-            if(snapshot.exists) {
+            if (snapshot.exists) {
                 const data = snapshot.data()
                 dispatch({
                     type: USER_INVITE_GET_SUCCESS,
                     payload: snapshot.data()
                 })
-                if(data.status === 'completed') {
+                if (data.status === 'completed') {
                     history.push(history.location.pathname + data.projectId)
                 }
             } else {
@@ -143,4 +170,62 @@ export const listenForInvite = inviteId => dispatch => {
 
 export const unsubscribeRealtimeInviteListener = () => () => {
     stopListenForInvite && stopListenForInvite()
+}
+
+export const getPendingInvites = () => (dispatch, getState) => {
+    const projectId = getSelectedProjectIdSelector(getState())
+    fireStoreMainInstance
+        .collection('projects-invites')
+        .where('projectId', '==', projectId)
+        .where('status', 'in', ['new', 'emailSent', 'error'])
+        .get()
+        .then(querySnapshot => {
+            const pendingInvite = []
+            querySnapshot.forEach(snapshot => {
+                pendingInvite.push({
+                    id: snapshot.id,
+                    ...snapshot.data()
+                })
+            })
+            return dispatch({
+                type: USER_INVITES_GET_SUCCESS,
+                payload: pendingInvite
+            })
+        })
+        .catch(error => {
+            // eslint-disable-next-line no-console
+            console.error(error)
+            dispatch({
+                type: ADD_NOTIFICATION,
+                payload: {
+                    type: 'error',
+                    message: `Failed to load pending invites, ${JSON.stringify(error)}`
+                }
+            })
+            return false
+        })
+}
+
+export const cancelInvite = (inviteId) => dispatch => {
+    fireStoreMainInstance
+        .collection('projects-invites')
+        .doc(inviteId)
+        .delete()
+        .then(() => dispatch({
+                type: USER_INVITE_REMOVE_SUCCESS,
+                payload: inviteId
+            })
+        )
+        .catch(error => {
+            // eslint-disable-next-line no-console
+            console.error(error)
+            dispatch({
+                type: ADD_NOTIFICATION,
+                payload: {
+                    type: 'error',
+                    message: `Failed to cancel invite, ${JSON.stringify(error)}`
+                }
+            })
+            return false
+        })
 }
