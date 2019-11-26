d9c4e2e4e2fb1146278daf3215f26252a9b0075c
Hugo Gresse <hugo.gresse@gmail.com>
2019-11-26 12:46:14 +0100

    Admin list refacto to use OFListHeader and OFListItem (#250)
    * Prevent creating alert outside of production
    
    * Refacto speaker list to use Grid
    
    * Refactor VoteItems to use Grid

--- src/admin/project/settings/votingForm/VoteItemList.js
+++ src/admin/project/settings/votingForm/VoteItemList.js
@@ -1,97 +1,66 @@
 import React from 'react'
-import Table from '@material-ui/core/Table'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import TableCell from '@material-ui/core/TableCell'
-import TableBody from '@material-ui/core/TableBody'
 import AddIcon from '@material-ui/icons/AddCircleOutline'
-import { withStyles } from '@material-ui/core'
 import VoteItem from './VoteItem'
 import { useDispatch, useSelector } from 'react-redux'
-import { getBooleanVoteItemsSelector, isSavingSelector } from './votingFormSelectors'
+import {
+    getBooleanVoteItemsSelector,
+    isSavingSelector,
+} from './votingFormSelectors'
 import {
     onVoteItemAddBoolean,
     onVoteItemChange,
     onVoteItemDelete,
     onVoteItemMoveDown,
     onVoteItemMoveUp,
-    saveVoteItems
+    saveVoteItems,
 } from './votingFormActions'
 import Button from '@material-ui/core/Button'
-import OFButton from '../../../baseComponents/OFButton'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Fade from '@material-ui/core/Fade'
+import Grid from '@material-ui/core/Grid'
+import OFListHeader from '../../../baseComponents/layouts/OFListHeader'
+import OFListItem from '../../../baseComponents/layouts/OFListItem'
 
-const styles = () => ({
-    icon: {
-        marginRight: 6
-    },
-    progress: {
-        width: 30,
-        height: 30,
-        marginRight: 10,
-        top: 10,
-        position: 'relative'
-    }
-})
-
-const VoteItemList = ({ classes }) => {
+const VoteItemList = () => {
     const dispatch = useDispatch()
     const voteItems = useSelector(getBooleanVoteItemsSelector)
     const isSaving = useSelector(isSavingSelector)
 
     return (
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Vote items</TableCell>
-                    <TableCell align="right">
-                        <Fade in={isSaving}>
-                            <CircularProgress
-                                size={30}
-                                className={classes.progress}
-                            />
-                        </Fade>
-
-                        <OFButton onClick={() => dispatch(saveVoteItems())}>
-                            Save
-                        </OFButton>
-                    </TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {voteItems.map(item => (
-                    <VoteItem
-                        key={item.id}
-                        item={item}
-                        onChange={newValue =>
-                            dispatch(
-                                onVoteItemChange({
-                                    ...item,
-                                    name: newValue
-                                })
-                            )
-                        }
-                        onMoveUp={() => dispatch(onVoteItemMoveUp(item))}
-                        onMoveDown={() => dispatch(onVoteItemMoveDown(item))}
-                        onDelete={() => dispatch(onVoteItemDelete(item))}
-                        onEnterPressed={() => dispatch(onVoteItemAddBoolean())}
-                    />
-                ))}
-                <TableRow>
-                    <TableCell component="th" scope="row">
-                        <Button
-                            aria-label="new vote item"
-                            onClick={() => dispatch(onVoteItemAddBoolean())}
-                        >
-                            <AddIcon className={classes.icon} />
-                            New item
-                        </Button>
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
+        <Grid container>
+            <OFListHeader
+                title="Vote items"
+                disableFilter={true}
+                buttonProcessing={isSaving}
+                buttonClick={() => dispatch(saveVoteItems())}
+                buttonText="Save"
+            />
+            {voteItems.map(item => (
+                <VoteItem
+                    key={item.id}
+                    item={item}
+                    onChange={newValue =>
+                        dispatch(
+                            onVoteItemChange({
+                                ...item,
+                                name: newValue,
+                            })
+                        )
+                    }
+                    onMoveUp={() => dispatch(onVoteItemMoveUp(item))}
+                    onMoveDown={() => dispatch(onVoteItemMoveDown(item))}
+                    onDelete={() => dispatch(onVoteItemDelete(item))}
+                    onEnterPressed={() => dispatch(onVoteItemAddBoolean())}
+                />
+            ))}
+            <OFListItem style={{ paddingLeft: 20, paddingRight: 20 }}>
+                <Button
+                    aria-label="new vote item"
+                    onClick={() => dispatch(onVoteItemAddBoolean())}>
+                    <AddIcon style={{ marginRight: 6 }} />
+                    New item
+                </Button>
+            </OFListItem>
+        </Grid>
     )
 }
 
-export default withStyles(styles)(VoteItemList)
+export default VoteItemList
--- src/admin/project/speakers/SpeakerListItem.js
+++ src/admin/project/speakers/SpeakerListItem.js
@@ -1,37 +1,63 @@
 import React from 'react'
-import TableRow from '@material-ui/core/TableRow'
-import TableCell from '@material-ui/core/TableCell'
 import EditIcon from '@material-ui/icons/Edit'
 import DeleteIcon from '@material-ui/icons/Delete'
 import IconButton from '@material-ui/core/IconButton'
-import OFInput from "../../baseComponents/OFInput"
+import OFListItem from '../../baseComponents/layouts/OFListItem'
+import Grid from '@material-ui/core/Grid'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import Avatar from '@material-ui/core/Avatar'
+import { Box } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
 
-const SpeakerListItem = ({item, onChange}) => {
+const useStyles = makeStyles(theme => ({
+    cell: {
+        paddingRight: 12,
+        [theme.breakpoints.down('sm')]: {
+            paddingTop: 12,
+        },
+    },
+    buttonCell: {
+        textAlign: 'right',
+    },
+}))
 
+const SpeakerListItem = ({ speaker }) => {
+    const classes = useStyles()
     const onButtonClick = () => {
-        alert('Speakers are read only here. You can probably update them through your Hoverboard Firestore or json url ' +
-            'depending on your setup.')
+        alert(
+            'Speakers are read only here. You can probably update them through your Hoverboard Firestore or json url ' +
+                'depending on your setup.'
+        )
     }
 
     return (
-        <TableRow>
-            <TableCell component="th" scope="row">
-                <OFInput
-                    value={item.name}
-                    disabled
-                    onChange={event => onChange(event.target.value)}
+        <OFListItem>
+            <Grid
+                item
+                xs={12}
+                sm={10}
+                className={classes.cell}
+                component={Box}
+                display="flex">
+                <Avatar
+                    alt={speaker.name}
+                    src={speaker.photoUrl}
+                    style={{ marginRight: 12, marginTop: 2 }}
                 />
-            </TableCell>
-            <TableCell align="right">
+                <Typography style={{ fontWeight: 600, lineHeight: '40px' }}>
+                    {speaker.name}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={2} className={classes.buttonCell}>
                 <IconButton aria-label="edit" onClick={() => onButtonClick()}>
-                    <EditIcon/>
+                    <EditIcon />
                 </IconButton>
 
                 <IconButton aria-label="delete" onClick={() => onButtonClick()}>
-                    <DeleteIcon/>
+                    <DeleteIcon />
                 </IconButton>
-            </TableCell>
-        </TableRow>
+            </Grid>
+        </OFListItem>
     )
 }
 
