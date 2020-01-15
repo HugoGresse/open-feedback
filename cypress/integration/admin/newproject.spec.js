c6525615a7baee2aa84e29c9e58b5d119286af5f
Hugo Gresse <hugo.gresse@gmail.com>
2020-01-15 14:53:04 +0100

    Delete duplicate spec + update the current

--- cypress/integration/admin/newproject.spec.js
+++ cypress/integration/admin/newproject.spec.js
@@ -76,4 +76,55 @@ describe('Test creating a new project', function() {
         cy.contains(data.speaker1.name).should('be.visible')
         cy.contains(data.speaker2.name).should('be.visible')
     })
+
+    it('New OpenFeedback project 2', function() {
+        cy.adminLogin()
+
+        cy.contains('Create a new event').click()
+
+        cy.get('input[type=text]').type(data.projectName)
+
+        cy.contains('Continue').click()
+
+        cy.get('input[value=openfeedbackv1]').check()
+
+        cy.contains('Create event').click()
+
+        cy.contains(data.projectName)
+
+        // Add a talk without speaker
+        cy.contains('Talks').click()
+        cy.contains('Add talks').click()
+        cy.get('input[name=title]').type(data.talk1Name)
+        cy.get('input[id=trackTitle]').type(data.track1)
+        cy.get('input[id=tags]').type(data.tag1)
+        cy.get('button[type=submit]')
+            .first()
+            .click()
+
+        // Edit the added talk to add 2 speaker
+        cy.contains(data.talk1Name)
+            .parent()
+            .parent()
+            .get('button[aria-label="edit"]')
+            .click()
+        cy.get('input[id=speakers]').type(data.speaker1.name)
+        cy.contains('Add a speaker').click()
+        cy.get('input[name=name]').type(data.speaker1.name)
+        cy.get('input[name=photoUrl]').type(data.speaker1.photoUrl)
+        cy.get('button[type=submit]')
+            .eq(2)
+            .click()
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(200)
+        cy.get('input[name=name]').type(data.speaker2.name)
+        cy.get('input[name=photoUrl]').type(data.speaker2.photoUrl)
+        cy.contains('Add speaker').click()
+        cy.contains('Save').click()
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(500)
+        cy.contains(data.talk1Name).should('be.visible')
+        cy.contains(data.speaker1.name).should('be.visible')
+        cy.contains(data.speaker2.name).should('be.visible')
+    })
 })
