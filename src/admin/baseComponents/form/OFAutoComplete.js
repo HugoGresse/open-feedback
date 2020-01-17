db91f60990f75c6666811770846a44af25af8abd
Hugo Gresse <hugo.gresse@gmail.com>
2020-01-17 17:12:45 +0100

    [Admin] Cypress test for voting form + assert displayed result + tag fix when enter not pressed (#345)
    * Fix voting form not being fetched correctly after creating a project
    
    * Order projects on createdAt field
    
    * Fix error in adding talk related to ForwardedRef
    
    * Add test on voting form + adding a new talk with exising data
    
    * Fix tags not being save if enter is not pressed on save
    
    * Improve cypress test
    
    * Fix test after emptying db...

--- src/admin/baseComponents/form/OFAutoComplete.js
+++ src/admin/baseComponents/form/OFAutoComplete.js
@@ -14,19 +14,40 @@ const AutoCompleteInput = styled(({ forwardedRef2, ...props }) => (
 
 const OFAutoComplete = ({ field, form, dataArray, keyToDisplay, ...other }) => {
     const getLabel = option => (keyToDisplay ? option[keyToDisplay] : option)
+    let inputChangeValue = null
 
     return (
         <Autocomplete
             {...other}
             options={dataArray}
+            value={field.value}
             getOptionLabel={option => getLabel(option)}
             id={field.name}
             disabled={!!form.isSubmitting}
             defaultValue={field.value}
+            blurOnSelect={true}
+            onClose={() => {
+                if (
+                    other.freeSolo &&
+                    other.multiple &&
+                    inputChangeValue &&
+                    inputChangeValue.length > 0 &&
+                    field.value.indexOf(inputChangeValue) === -1
+                ) {
+                    // If the field can have arbitrary value (freeSolo) && can have multiple value, when the field is
+                    // blurred, we update the value
+                    form.setFieldValue(
+                        field.name,
+                        [...field.value, inputChangeValue],
+                        false
+                    )
+                }
+            }}
             onChange={(event, elements) =>
                 form.setFieldValue(field.name, elements, false)
             }
             onInputChange={(event, value) => {
+                inputChangeValue = value
                 if (other.freeSolo && !other.multiple) {
                     form.setFieldValue(field.name, value, false)
                 }
--- src/admin/project/core/projectSelectors.js
+++ src/admin/project/core/projectSelectors.js
@@ -23,10 +23,10 @@ export const getSortedProjectsSelector = createSelector(
     getProjectsData,
     projectsData => {
         return projectsData.projects.sort((a, b) => {
-            if (a.name < b.name) {
+            if (a.createdAt < b.createdAt) {
                 return 1
             }
-            if (a.name > b.name) {
+            if (a.createdAt > b.createdAt) {
                 return -1
             }
             return 0
