9942e66439819906a36c325a95cd87854ea2a795
Hugo Gresse <hugo.gresse@gmail.com>
2020-01-25 13:52:01 +0100

    Add Suspens on QRCode (was using just lazy before)

--- src/admin/project/layout/QRCodeDialog.js
+++ src/admin/project/layout/QRCodeDialog.js
@@ -1,10 +1,11 @@
-import React, { lazy } from 'react'
+import React, { lazy, Suspense } from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogActions from '@material-ui/core/DialogActions'
 import OFButton from '../../baseComponents/OFButton'
 import GetAppIcon from '@material-ui/icons/GetApp'
 import FileSaver from 'file-saver'
+import LoaderMatchParent from '../../../baseComponents/customComponent/LoaderMatchParent'
 const QRCode = lazy(() => import('qrcode.react'))
 
 const QRCodeDialog = ({ open, handleClose, data, name }) => {
@@ -27,11 +28,13 @@ const QRCodeDialog = ({ open, handleClose, data, name }) => {
             open={open}
             id="dialog-qrcode">
             <DialogContent>
-                <QRCode
-                    value={data}
-                    size={300}
-                    style={{ maxWidth: '100%', height: 'auto' }}
-                />
+                <Suspense fallback={<LoaderMatchParent />}>
+                    <QRCode
+                        value={data}
+                        size={300}
+                        style={{ maxWidth: '100%', height: 'auto' }}
+                    />
+                </Suspense>
             </DialogContent>
             <DialogActions style={{ justifyContent: 'space-between' }}>
                 <OFButton onClick={downloadImage}>
