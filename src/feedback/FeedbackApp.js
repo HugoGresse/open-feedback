caa04c369c85d9b0cecbc3bfd9fac30739b970b5
Hugo Gresse <hugo.gresse@gmail.com>
2020-02-01 18:04:10 +0100

    Add translation for feedback app

--- src/feedback/FeedbackApp.js
+++ src/feedback/FeedbackApp.js
@@ -4,37 +4,41 @@ import React from 'react'
 import AppLayout from './AppLayout'
 import TalksListWrapper from './talks/TalksListWrapper'
 import Talk from './talk/Talk'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './translations/i18n'
 
 const FeedbackApp = () => {
     return (
-        <Switch>
-            <Route
-                exact
-                path="/:projectId"
-                render={props => (
-                    <AppLayout {...props}>
-                        <TalksListWrapper {...props} />
-                    </AppLayout>
-                )}
-            />
-            <Route
-                exact
-                path="/:projectId/:date"
-                render={props => (
-                    <AppLayout {...props}>
-                        <TalksListWrapper {...props} />
-                    </AppLayout>
-                )}
-            />
-            <Route
-                path="/:projectId/:date/:talkId"
-                render={props => (
-                    <AppLayout {...props}>
-                        <Talk {...props} />
-                    </AppLayout>
-                )}
-            />
-        </Switch>
+        <I18nextProvider i18n={i18n}>
+            <Switch>
+                <Route
+                    exact
+                    path="/:projectId"
+                    render={props => (
+                        <AppLayout {...props}>
+                            <TalksListWrapper {...props} />
+                        </AppLayout>
+                    )}
+                />
+                <Route
+                    exact
+                    path="/:projectId/:date"
+                    render={props => (
+                        <AppLayout {...props}>
+                            <TalksListWrapper {...props} />
+                        </AppLayout>
+                    )}
+                />
+                <Route
+                    path="/:projectId/:date/:talkId"
+                    render={props => (
+                        <AppLayout {...props}>
+                            <Talk {...props} />
+                        </AppLayout>
+                    )}
+                />
+            </Switch>
+        </I18nextProvider>
     )
 }
 
