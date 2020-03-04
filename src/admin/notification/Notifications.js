06a6563f4c37305a38db2a61757fef9b0cd6e8d9
Hugo Gresse <hugo.gresse@gmail.com>
2020-03-04 14:29:23 +0100

    Refactor Notification to function component

--- src/admin/notification/Notifications.js
+++ src/admin/notification/Notifications.js
@@ -1,113 +1,95 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import SnackbarContent from '@material-ui/core/SnackbarContent'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 import ErrorIcon from '@material-ui/icons/Error'
-import withStyles from '@material-ui/core/styles/withStyles'
 import green from '@material-ui/core/colors/green'
 import amber from '@material-ui/core/colors/amber'
 import { getLastNotificationsSelector } from './notificationSelectors'
 import Snackbar from '@material-ui/core/Snackbar'
 import { clearNotification } from './notifcationActions'
+import makeStyles from '@material-ui/core/styles/makeStyles'
 
 const variantIcon = {
     success: CheckCircleIcon,
-    error: ErrorIcon
+    error: ErrorIcon,
 }
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     success: {
-        backgroundColor: green[600]
+        backgroundColor: green[600],
     },
     error: {
-        backgroundColor: theme.palette.error.dark
+        backgroundColor: theme.palette.error.dark,
     },
     info: {
-        backgroundColor: theme.palette.primary.dark
+        backgroundColor: theme.palette.primary.dark,
     },
     warning: {
-        backgroundColor: amber[700]
+        backgroundColor: amber[700],
     },
     icon: {
         fontSize: 20,
 
         opacity: 0.9,
-        marginRight: theme.spacing(1)
+        marginRight: theme.spacing(1),
     },
     iconVariant: {
         opacity: 0.9,
-        marginRight: theme.spacing(1)
+        marginRight: theme.spacing(1),
     },
     message: {
         display: 'flex',
-        alignItems: 'center'
-    }
-})
+        alignItems: 'center',
+    },
+}))
 
-class Notifications extends Component {
-    onNotificationClose(notification) {
-        this.props.clearNotification(notification)
-    }
+const Notifications = () => {
+    const classes = useStyles()
+    const notification = useSelector(getLastNotificationsSelector)
+    const dispatch = useDispatch()
 
-    render() {
-        const { classes, notification } = this.props
-        if (!notification) {
-            return ''
-        }
-        const Icon = variantIcon[notification.type]
-        const contentClass = classes[notification.type]
-
-        return (
-            <Snackbar
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right'
-                }}
-                open={!!notification}
-                autoHideDuration={6000}
-                onClose={() => this.onNotificationClose(notification)}
-            >
-                <SnackbarContent
-                    className={contentClass}
-                    aria-describedby="client-snackbar"
-                    message={
-                        <span id="client-snackbar" className={classes.message}>
-                            {' '}
-                            <Icon className={classes.icon} />{' '}
-                            {notification.message}{' '}
-                        </span>
-                    }
-                    action={[
-                        <IconButton
-                            key="close"
-                            aria-label="Close"
-                            color="inherit"
-                            className={classes.close}
-                            onClick={() => this.onNotificationClose(notification)}
-                        >
-                            <CloseIcon className={classes.icon} />
-                        </IconButton>
-                    ]}
-                />
-            </Snackbar>
-        )
+    if (!notification) {
+        return ''
     }
+    const Icon = variantIcon[notification.type]
+    const contentClass = classes[notification.type]
+
+    return (
+        <Snackbar
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            open={!!notification}
+            autoHideDuration={4000}
+            onClose={() => dispatch(clearNotification(notification))}>
+            <SnackbarContent
+                className={contentClass}
+                aria-describedby="client-snackbar"
+                message={
+                    <span id="client-snackbar" className={classes.message}>
+                        {' '}
+                        <Icon className={classes.icon} /> {notification.message}{' '}
+                    </span>
+                }
+                action={[
+                    <IconButton
+                        key="close"
+                        aria-label="Close"
+                        color="inherit"
+                        className={classes.close}
+                        onClick={() =>
+                            dispatch(clearNotification(notification))
+                        }>
+                        <CloseIcon className={classes.icon} />
+                    </IconButton>,
+                ]}
+            />
+        </Snackbar>
+    )
 }
 
-const mapStateToProps = state => ({
-    notification: getLastNotificationsSelector(state)
-})
-
-const mapDispatchToProps = Object.assign(
-    {},
-    {
-        clearNotification: clearNotification
-    }
-)
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withStyles(styles)(Notifications))
+export default Notifications
