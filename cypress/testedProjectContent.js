8b5b84edf70efe3be8fa6452adcccfe738b6bcb9
Hugo Gresse <hugo.gresse@gmail.com>
2020-03-05 10:15:46 +0100

    Add test project sample data

---
+++ cypress/testedProjectContent.js
@@ -0,0 +1,43 @@
+const project = {
+    chipColors: ['c67d10'],
+    config: {
+        jsonUrl:
+            'https://gist.githubusercontent.com/HugoGresse/8503843cb0850e7c90efade86b73d6f6/raw/f72c292925c05ae963398cd0354c71db13612b22/data.json',
+    },
+    createdAt: {
+        seconds: 1579273261,
+        nanoseconds: 698000000,
+    },
+    favicon: 'http://localhost:3000/favicon-32x32.png',
+    logoSmall: 'http://localhost:3000/android-chrome-192x192.png',
+    members: ['C8sblCQv0WQNi2aJWUytTSZOdLJ2'],
+    name: 'Cypress vote test',
+    owner: 'C8sblCQv0WQNi2aJWUytTSZOdLJ2',
+    setupType: 'jsonurl',
+    voteItems: [
+        {
+            id: 'AnETPhIVKca6yeo0jlar',
+            name: 'Comment',
+            type: 'text',
+        },
+        {
+            id: 's8X4382bCSLCa6NlMUOM',
+            name: 'Trop de code',
+            position: 0,
+            type: 'boolean',
+        },
+        {
+            id: '7dzTDy0u0717SFPePRlV',
+            name: 'Super intéressant',
+            position: 1,
+            type: 'boolean',
+        },
+        {
+            id: 'auBMVilR8K49dYduob26',
+            name: 'Une petite pépite',
+            position: 2,
+            type: 'boolean',
+        },
+    ],
+    id: 'dfXTjztKc9h1MJHBaxDr',
+}
