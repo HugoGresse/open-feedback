f9251c15a09af29cfff16074c3e58a914a79fd4b
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-09 15:20:38 +0200

    Remove old languages used in voteItems used project save

--- src/admin/project/core/actions/editProject.js
+++ src/admin/project/core/actions/editProject.js
@@ -6,22 +6,25 @@ import { deleteField, fireStoreMainInstance } from '../../../../firebase'
 import { addNotification } from '../../../notification/notifcationActions'
 import { EDIT_PROJECT_ERROR, EDIT_PROJECT_SUCCESS } from '../projectActionTypes'
 import { deleteOldFilesIfNewValueDiffer } from '../../utils/storage/deleteImageIfPossible'
+import { getSortedVoteItemsSelector } from '../../settings/votingForm/votingFormSelectors'
+import { getVoteItems } from '../../settings/votingForm/votingFormActions'
 
 export const editProject = (projectData) => (dispatch, getState) => {
     const currentProject = getSelectedProjectSelector(getState())
-    if (
-        !projectData.restrictVoteRange &&
-        projectData.restrictVoteRange !== undefined
-    ) {
-        projectData.voteStartTime = deleteField()
-        projectData.voteEndTime = deleteField()
+
+    dispatch(getVoteItems())
+    const fixedProject = {
+        ...fixProjectVoteRange(projectData),
+        ...fixVoteItemLanguages(
+            projectData.languages,
+            getSortedVoteItemsSelector(getState())
+        ),
     }
-    delete projectData.restrictVoteRange
 
     return fireStoreMainInstance
         .collection('projects')
         .doc(getSelectedProjectIdSelector(getState()))
-        .set(projectData, { merge: true })
+        .set(fixedProject, { merge: true })
         .then(() => {
             dispatch(
                 addNotification({
@@ -54,4 +57,63 @@ export const editProject = (projectData) => (dispatch, getState) => {
                 payload: err.toString(),
             })
         })
+}
+
+/**
+ * Mutate project object base on vote range change. When restricted vote date are unselected from an event, we need to
+ * remove the voteEndTime and voteStartTime from the DB.
+ * @param project
+ */
+const fixProjectVoteRange = (project) => {
+    const editedValues = {
+        ...project,
+    }
+    if (
+        !editedValues.restrictVoteRange &&
+        editedValues.restrictVoteRange !== undefined
+    ) {
+        editedValues.voteStartTime = deleteField()
+        editedValues.voteEndTime = deleteField()
+    }
+    delete editedValues.restrictVoteRange
+    return editedValues
+}
+
+/**
+ * We need to cleanup vote items when an additional language has been added, used and then removed from an event.
+ *
+ * @param languages project languages
+ * @param voteItems vote items of a project
+ * @returns {{voteItems: *}|{}}
+ */
+const fixVoteItemLanguages = (languages, voteItems) => {
+    if (!languages || !voteItems || voteItems.length === 0) {
+        return {}
+    }
+
+    const voteItemsLangs = [
+        ...new Set(
+            voteItems.reduce((acc, item) => {
+                if (item.languages) {
+                    return acc.concat(Object.keys(item.languages))
+                }
+                return acc
+            }, [])
+        ),
+    ]
+    const undesiredLangs = voteItemsLangs.filter(
+        (lang) => !languages.includes(lang)
+    )
+
+    return {
+        voteItems: voteItems.map((item) => {
+            const cloneItem = { ...item }
+            if (item.languages && Object.keys(item.languages > 0)) {
+                undesiredLangs.forEach((undesiredLang) => {
+                    delete cloneItem.languages[undesiredLang]
+                })
+            }
+            return cloneItem
+        }),
+    }
 }
