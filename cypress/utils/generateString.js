0c6594a9189f9c440c2986db9e790d997b1bba63
Hugo Gresse <hugo.gresse@gmail.com>
2021-01-13 23:08:07 +0100

    Fix generateString

--- cypress/utils/generateString.js
+++ cypress/utils/generateString.js
@@ -1,4 +1,3 @@
 export const stringGenerator = (long = false) =>
-    Math.random().toString(36).substring(2, 15) + long
-        ? ' ' + Math.random().toString(36).substring(2, 15)
-        : ''
+    Math.random().toString(36).substring(2, 15) +
+    (long ? ' ' + stringGenerator() : '')
