5734c9fe59d4f429cf0cbbf413560e27b8eac57b
Hugo Gresse <hugo.gresse@gmail.com>
2021-01-24 13:43:38 +0100

    Fix undefined talks or speakers from json

--- src/core/speakers/speakerSelectors.js
+++ src/core/speakers/speakerSelectors.js
@@ -1,17 +1,18 @@
-import { createSelector } from "reselect"
-import { normalizeAndRemoveDiacritics } from "../../utils/stringUtils"
+import { createSelector } from 'reselect'
+import { normalizeAndRemoveDiacritics } from '../../utils/stringUtils'
 
-export const getSpeakersSelector = state => state.speakers
+export const getSpeakersSelector = (state) => state.speakers
 
-export const getSpeakersListSelector = state => getSpeakersSelector(state).list
+export const getSpeakersListSelector = (state) =>
+    getSpeakersSelector(state).list || {}
 
-export const getSpeakersFilter = state => getSpeakersSelector(state).filter
+export const getSpeakersFilter = (state) => getSpeakersSelector(state).filter
 
 //  MEMOIZED SELECTORS HERE
 
 export const getSpeakersAsArraySelector = createSelector(
     getSpeakersListSelector,
-    speakers => {
+    (speakers) => {
         return Object.keys(speakers).reduce((acc, id) => {
             acc.push(speakers[id])
             return acc
@@ -23,10 +24,16 @@ export const getFilteredSpeakers = createSelector(
     getSpeakersAsArraySelector,
     getSpeakersFilter,
     (speakers, filter) => {
-        if(!filter) {
+        if (!filter) {
             return speakers
         }
-        const cleanedFilter = normalizeAndRemoveDiacritics(filter.trim().toLowerCase())
-        return speakers.filter(speaker => normalizeAndRemoveDiacritics(speaker.name.toLowerCase()).includes(cleanedFilter))
+        const cleanedFilter = normalizeAndRemoveDiacritics(
+            filter.trim().toLowerCase()
+        )
+        return speakers.filter((speaker) =>
+            normalizeAndRemoveDiacritics(speaker.name.toLowerCase()).includes(
+                cleanedFilter
+            )
+        )
     }
 )
