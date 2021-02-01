5dd09f2aabefe34fc98c42d67813e7dfa483a653
Hugo Gresse <hugo.gresse@gmail.com>
2021-02-01 21:47:16 +0100

    Remove serviceAccountKey from functions (or try to) (#1022)
    * Revert "Revert commit Add save shortcut to voting form"
    
    This reverts commit 7bd7d1d7
    
    * Revert "Revert "Add translation info in voting form""
    
    This reverts commit a33ad763
    
    * Add save shortcut to event&theme page
    
    * Add test and allow localhost image url on event
    
    * Add second save button in voting form
    
    * Fix test
    
    * Remove serviceAccountKey (or try too) from functions
    
    * Fix date selector once and fore all

--- functions/src/helpers/firebaseInit.ts
+++ functions/src/helpers/firebaseInit.ts
@@ -1,16 +1,7 @@
 import * as admin from 'firebase-admin'
 
 export const initFirebase = (env: string) => {
-    // eslint-disable-next-line no-undef
-    const serviceAccount = require(env === 'development'
-        ? '../../serviceAccountKey.development.json'
-        : '../../serviceAccountKey.production.json')
-
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
-        storageBucket: `${serviceAccount.project_id}.appspot.com`,
-    })
+    admin.initializeApp()
 }
 
 export const arrayUnion = admin.firestore.FieldValue.arrayUnion
