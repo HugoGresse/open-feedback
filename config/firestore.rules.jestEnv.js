2cd78c904ca883f84fc20e000a4e8a3c70d5034e
Hugo Gresse <hugo.gresse@gmail.com>
2021-02-06 22:02:08 +0100

    WIP on security rules

---
+++ config/firestore.rules.jestEnv.js
@@ -0,0 +1,23 @@
+// Stolen from: https://github.com/ipfs/jest-environment-aegir/blob/master/src/index.js
+// Overcomes error from jest internals.. this thing: https://github.com/facebook/jest/issues/6248
+const NodeEnvironment = require('jest-environment-node')
+
+class MyEnvironment extends NodeEnvironment {
+    constructor(config) {
+        super({
+            ...config,
+            globals: {
+                ...config.globals,
+                Uint32Array: Uint32Array,
+                Uint8Array: Uint8Array,
+                ArrayBuffer: ArrayBuffer,
+            },
+        })
+    }
+
+    async setup() {}
+
+    async teardown() {}
+}
+
+module.exports = MyEnvironment
