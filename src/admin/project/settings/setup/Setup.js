8f20c6344682cba88a0bdeaf3f942efed90a84cb
Hugo Gresse <hugo.gresse@gmail.com>
2022-06-18 13:29:16 +0200

    Add live user vote in admin settings

--- src/admin/project/settings/setup/Setup.js
+++ src/admin/project/settings/setup/Setup.js
@@ -35,6 +35,7 @@ const Setup = () => {
                             hideVotesUntilUserVote={
                                 project.hideVotesUntilUserVote
                             }
+                            liveUserVotes={project.liveUserVotes}
                         />
                     </Suspense>
                 </CardContent>
