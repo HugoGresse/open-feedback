d91ca31aa9d5ed2c6283614911d93d97ed2b287a
Hugo Gresse <hugo.gresse@gmail.com>
2022-12-21 14:19:19 +0100

    Fix text vote delete after save after delete

--- src/feedback/talk/TalkVote.js
+++ src/feedback/talk/TalkVote.js
@@ -1,54 +1,38 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import TalkVoteBoolean from './boolean/TalkVoteBoolean'
 import { TalkVoteText } from './text/TalkVoteText'
 import { VOTE_TYPE_BOOLEAN, VOTE_TYPE_TEXT } from '../../core/contants'
 
-class TalkItemVote extends Component {
-    render() {
-        const {
-            voteItem,
-            userVotes,
-            voteResult,
-            chipColors,
-            onVoteChange,
-        } = this.props
-
-        switch (voteItem.type) {
-            default:
-            case VOTE_TYPE_BOOLEAN:
-                return (
-                    <TalkVoteBoolean
-                        onVoteChange={(voteItem) => {
-                            onVoteChange(
-                                voteItem,
-                                null,
-                                userVotes && userVotes[0]
-                            )
-                        }}
-                        voteItem={voteItem}
-                        isSelected={!!userVotes}
-                        voteResult={voteResult}
-                        chipColors={chipColors}
-                    />
-                )
-            case VOTE_TYPE_TEXT:
-                return (
-                    <TalkVoteText
-                        onVoteChange={onVoteChange}
-                        voteItem={voteItem}
-                        currentUserVotes={userVotes}
-                        voteResult={voteResult}
-                        chipColors={chipColors}
-                    />
-                )
-        }
+export const TalkVote = ({
+    voteItem,
+    userVotes,
+    voteResult,
+    chipColors,
+    onVoteChange,
+}) => {
+    switch (voteItem.type) {
+        default:
+        case VOTE_TYPE_BOOLEAN:
+            return (
+                <TalkVoteBoolean
+                    onVoteChange={(voteItem) => {
+                        onVoteChange(voteItem, null, userVotes && userVotes[0])
+                    }}
+                    voteItem={voteItem}
+                    isSelected={!!userVotes}
+                    voteResult={voteResult}
+                    chipColors={chipColors}
+                />
+            )
+        case VOTE_TYPE_TEXT:
+            return (
+                <TalkVoteText
+                    onVoteChange={onVoteChange}
+                    voteItem={voteItem}
+                    currentUserVotes={userVotes}
+                    voteResult={voteResult}
+                    chipColors={chipColors}
+                />
+            )
     }
 }
-
-TalkItemVote.propTypes = {
-    voteItem: PropTypes.object.isRequired,
-    chipColors: PropTypes.array,
-}
-
-export default TalkItemVote
--- src/feedback/talk/text/TalkVoteText.js
+++ src/feedback/talk/text/TalkVoteText.js
@@ -81,8 +81,8 @@ export const TalkVoteText = ({
         dataLoaded: false,
     })
 
-    useEffect(() => {
-        if (currentUserVotes && !data.dataLoaded) {
+    const getVoteFromCurrentVotes = () => {
+        if (currentUserVotes && (!data.dataLoaded || !data.vote)) {
             const foundTextVote = currentUserVotes.find(
                 (vote) => !!vote.text && vote.voteType !== VOTE_TYPE_TEXT_PLUS
             )
@@ -95,7 +95,11 @@ export const TalkVoteText = ({
                 })
             }
         }
-    }, [currentUserVotes, data.dataLoaded])
+    }
+
+    useEffect(() => {
+        getVoteFromCurrentVotes()
+    }, [currentUserVotes, data.dataLoaded, getVoteFromCurrentVotes])
 
     const onTextChange = (event) => {
         setData({
@@ -105,14 +109,15 @@ export const TalkVoteText = ({
     }
 
     const onVoteDelete = () => {
-        onVoteChange(voteItem, null, data.vote)
-
         setData({
             ...data,
             comment: '',
             status: null,
             vote: null,
         })
+        if (data.vote) {
+            onVoteChange(voteItem, null, data.vote)
+        }
     }
 
     const saveUpdateKey =
