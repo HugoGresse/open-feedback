6ea8fc954f90101f1e56fbde71675ba37ed23531
Hugo Gresse <hugo.gresse@gmail.com>
2024-05-19 19:11:58 +0200

    Change the chrome launch option for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,5 +14,21 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3000',
         specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+
+        setupNodeEvents(on) {
+            on('before:browser:launch', (browser, launchOptions) => {
+                if (browser.family === 'chromium') {
+                    // running headless chrome in a virtualized environment forces pointer type to default to `NONE`
+                    // to mimic "desktop" environment more correctly we force blink to have `pointer: fine` support
+                    // this allows correct pickers behavior.
+                    // This impact the used DateTimePicker in Material UI (MUI) between DesktopDateTimePicker and MobileDateTimePicker
+                    launchOptions.args.push(
+                        '--blink-settings=primaryPointerType=4'
+                    )
+                }
+
+                return launchOptions
+            })
+        },
     },
 })
